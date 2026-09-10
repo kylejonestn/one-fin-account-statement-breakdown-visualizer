2ffde99bd76fc0d735255109204c72d614c06b9b
Kyle Jones <kylejones@Kyles-Mac-mini.local>
2026-09-09 21:15:32 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/one-fin-account-statement-breakdown-visualizer/',
   plugins: [react(), tailwindcss()],
 })
